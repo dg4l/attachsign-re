be7f9b72a979656c87a507fc3ee802d619008d36
dg4l <79771590+dg4l@users.noreply.github.com>
2025-05-28 21:03:30 -0400

    retab

--- src/loadfile.h
+++ src/loadfile.h
@@ -1,48 +1,48 @@
 int loadFile(char *input, char **bufptr, size_t *size){
-  FILE *infile;
-  int ret;
-  size_t num;
-  size_t pos;
-  char *buf;
-  
-  if (!input) {
-    ret = -1;
-  }
-  else {
-    buf = 0;
-    pos = 0;
-    num = 0;
-    infile = fopen(input,"rb");
-    if (infile) {
-      fseek(infile,0,2);
-      pos = ftell(infile);
-      fseek(infile,0,0);
-      buf = malloc(pos);
-      if (buf) {
-        num = fread(buf,1,pos,infile);
-      }
-      fclose(infile);
-    }
-    if ((!buf) || (!pos) || (pos != num)) {
-      if (buf) {
-        free(buf);
-      }
-      buf = 0;
-      pos = 0;
-    }
-    if (bufptr) {
-      *bufptr = buf;
-    }
-    if (size) {
-      *size = pos;
-    }
-    if (!pos) {
-      ret = -1;
+    FILE *infile;
+    int ret;
+    size_t num;
+    size_t pos;
+    char *buf;
+    
+    if (!input) {
+        ret = -1;
     }
     else {
-      ret = 0;
+        buf = 0;
+        pos = 0;
+        num = 0;
+        infile = fopen(input,"rb");
+        if (infile) {
+            fseek(infile,0,2);
+            pos = ftell(infile);
+            fseek(infile,0,0);
+            buf = malloc(pos);
+            if (buf) {
+                num = fread(buf,1,pos,infile);
+            }
+            fclose(infile);
+        }
+        if ((!buf) || (!pos) || (pos != num)) {
+            if (buf) {
+                free(buf);
+            }
+            buf = 0;
+            pos = 0;
+        }
+        if (bufptr) {
+            *bufptr = buf;
+        }
+        if (size) {
+            *size = pos;
+        }
+        if (!pos) {
+            ret = -1;
+        }
+        else {
+            ret = 0;
+        }
     }
-  }
-  return ret;
+    return ret;
 }
 
--- src/savefile.h
+++ src/savefile.h
@@ -1,36 +1,36 @@
 int saveFile(char *out,char *buf,size_t size){
-  FILE *outfile;
-  int ret;
-  size_t num_bytes_written;
-  
-  if (!out) {
-    ret = -1;
-  }
-  else if (!buf) {
-    ret = -1;
-  }
-  else if (!size) {
-    ret = -1;
-  }
-  else {
-    num_bytes_written = 0;
-    outfile = fopen(out,"wb");
-    if (outfile) {
-      if (buf) {
-        num_bytes_written = fwrite(buf,1,size,outfile);
-      }
-      fclose(outfile);
+    FILE *outfile;
+    int ret;
+    size_t num_bytes_written;
+    
+    if (!out) {
+        ret = -1;
     }
-    if (size != num_bytes_written) {
-      size = 0;
+    else if (!buf) {
+        ret = -1;
     }
-    if (!size) {
-      ret = -1;
+    else if (!size) {
+        ret = -1;
     }
     else {
-      ret = 0;
+        num_bytes_written = 0;
+        outfile = fopen(out,"wb");
+        if (outfile) {
+            if (buf) {
+                num_bytes_written = fwrite(buf,1,size,outfile);
+            }
+            fclose(outfile);
+        }
+        if (size != num_bytes_written) {
+            size = 0;
+        }
+        if (!size) {
+            ret = -1;
+        }
+        else {
+            ret = 0;
+        }
     }
-  }
-  return ret;
+    return ret;
 }
 
